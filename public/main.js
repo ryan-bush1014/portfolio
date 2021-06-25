d2e4426dd72a028d86abb8194e78880f6f2aa1bb
Ryan <ryan.bush1014@gmail.com>
2021-06-25 11:41:03 -0400

    ignored

--- public/main.js
+++
@@ -1,7 +0,0 @@
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    console.log("Loading particles.js...")
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', 'particlesjs-config.json', function () {
-        console.log('Particles.js config loaded');
-    });
-}
